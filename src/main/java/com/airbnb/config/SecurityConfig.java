4441a28d2b4abcc6d821de5b4902f853da41fb6d
Ravi <coderaviraj@gmail.com>
2024-10-02 16:54:35 +0530

    Trying encrypting password done using Bcrypt

--- src/main/java/com/airbnb/config/SecurityConfig.java
+++ src/main/java/com/airbnb/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.airbnb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -8,8 +10,12 @@ public class SecurityConfig {
     // Configure Spring Security here.
     // For example, you can enable basic authentication and add custom login/logout endpoints.
 
-//    public SecurityFilterChain securityFilterChain() {
-//
-//
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //h(cd)^2
+        http.csrf().disable().cors().disable();
+    //haas.configure
+        http.authorizeHttpRequests().anyRequest().permitAll();
+        return http.build();
+    }
 }
--- src/main/java/com/airbnb/service/AppUserServiceImpl.java
+++ src/main/java/com/airbnb/service/AppUserServiceImpl.java
@@ -2,26 +2,22 @@ package com.airbnb.service;
 
 import com.airbnb.entity.AppUser;
 import com.airbnb.repository.AppUserRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppUserServiceImpl {
 
-    private PasswordEncoder passwordEncoder;
 
     private AppUserRepository appUserRepository;
 
-    public AppUserServiceImpl(PasswordEncoder passwordEncoder, AppUserRepository appUserRepository) {
-        this.passwordEncoder = passwordEncoder;
+    public AppUserServiceImpl(AppUserRepository appUserRepository) {
         this.appUserRepository = appUserRepository;
     }
 
     public AppUser createUser(AppUser user) {
 
-        String hashpw = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(3));
+        String hashpw = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10));
         user.setPassword(hashpw);
         return appUserRepository.save(user);
 
