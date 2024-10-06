ad33a0c73e39dc09e69297afc987ca36faa4996c
Ravi <coderaviraj@gmail.com>
2024-10-06 20:28:47 +0530

    Sign in using username or email

---
+++ src/main/java/com/airbnb/payload/LoginDto.java
@@ -0,0 +1,12 @@
+package com.airbnb.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDto {
+
+    public String username;
+    public String password;
+}
--- src/main/java/com/airbnb/service/AppUserServiceImpl.java
+++ src/main/java/com/airbnb/service/AppUserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.airbnb.service;
 
 import com.airbnb.entity.AppUser;
+import com.airbnb.payload.LoginDto;
 import com.airbnb.repository.AppUserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AppUserServiceImpl {
@@ -21,5 +26,16 @@ public class AppUserServiceImpl {
         user.setPassword(hashpw);
         return appUserRepository.save(user);
 
+    }
+
+    public boolean verifyLogin(LoginDto loginDto) {
+//        Optional<AppUser> opUser = appUserRepository.findByUsername(loginDto.getUsername());
+        Optional<AppUser> opUser = appUserRepository.findByUsernameOrEmail(loginDto.getUsername(), loginDto.getUsername());
+        if(opUser.isPresent()) {
+            AppUser appUser = opUser.get();
+            return BCrypt.checkpw(loginDto.getPassword(),appUser.getPassword());
+
+        }
+            return false;
     }
 }
